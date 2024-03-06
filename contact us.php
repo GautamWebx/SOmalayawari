2e473fc144240388a9ac26b50eb5cddfe74a0916
GautamWebx <162425362+GautamWebx@users.noreply.github.com>
2024-03-06 14:56:17 +0530

    Add files via upload

---
+++ contact us.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    call me
+</body>
+</html>
+.footer-form .input-field {
+  background: var(--white);
+  font-size: var(--fs-5);
+  padding: 15px 20px;
+  border-radius: 100px;
+  margin-bottom: 10px;
+}
+
+.footer-form .btn { width: 100%; }
+
+
+
